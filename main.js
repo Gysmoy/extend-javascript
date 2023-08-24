07b37996df20ad2dded16cf846b92e3d58f7e5a7
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2023-08-24 11:38:23 -0500

    NTS

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const Cookies = require('./sources/cookies')
 const Notify = require('./sources/notify')
 const storage = require('./sources/storage')
+const GET = require('./sources/get')
 
 module.exports = {
     Local: storage.Local,
     Session: storage.Session,
-    Cookies: Cookies,
-    Notify: Notify
+    Cookies,
+    Notify,
+    GET
 }
---
+++ sources/get.js
@@ -0,0 +1,6 @@
+const GET  = {}
+
+let urlParams = new URLSearchParams(location.search);
+[...urlParams].forEach(get => GET[get[0]] = get[1]);
+
+module.exports = GET
