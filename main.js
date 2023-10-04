3fffcb1f7c3b166c5b0c2f3ccadffa4e1494c185
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2023-10-04 08:45:04 -0500

    NTS

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const Cookies = require('./sources/cookies')
 const GET = require('./sources/get')
 const JSON = require('./sources/json')
 const Math = require('./sources/math')
+const String = require('./sources/string')
 const Notify = require('./sources/notify')
 const storage = require('./sources/storage')
 const _fetch = require('./sources/fetch')
@@ -17,5 +18,6 @@ module.exports = {
     Math,
     Clipboard,
     FetchParams: _fetch.FetchParams,
-    Fetch: _fetch.Fetch
+    Fetch: _fetch.Fetch,
+    String
 }
